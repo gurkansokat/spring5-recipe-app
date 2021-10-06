c0589df82c33877e7992463c43ed804870acc2c0
gsokat <gsokat@barracuda.com>
2021-10-05 23:56:33 -0700

    First JUnit Testing

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.domain;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    private static Category category;
+
+    @BeforeAll
+    public static void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4l;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
